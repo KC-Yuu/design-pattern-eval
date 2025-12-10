8b9e1bb8933de856eea4839da527170d6fee7ec0
KC-Yuu <maxime.caron2305@gmail.com>
2025-12-10 16:09:09 +0100

    ⚡️Now rooms manage their notifications

--- Room.js
+++ Room.js
@@ -2,9 +2,15 @@ export default class Room {
     constructor(name) {
         this.name = name;
         this.sensors = [];
+        this.notifiers = [];
+    }
+
+    addNotifier(notifier) {
+        this.notifiers.push(notifier);
     }
 
     addSensor(sensor) {
+        this.notifiers.forEach(notifier => sensor.subscribe(notifier));
         this.sensors.push(sensor);
     }
 
